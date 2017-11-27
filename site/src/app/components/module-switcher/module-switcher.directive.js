a187c21759e0cb38b5a61d135d2c790b906d9547
Dave Gramlich <callmehiphop@gmail.com>
2017-11-27 09:23:44 -0500

    site: add support for redirecting to new docs

--- site/src/app/components/module-switcher/module-switcher.directive.js
+++ site/src/app/components/module-switcher/module-switcher.directive.js
@@ -6,16 +6,28 @@
     .directive('moduleSwitcher', moduleSwitcher);
 
   /** @ngInject */
-  function moduleSwitcher($stateParams, manifest, util) {
+  function moduleSwitcher($state, manifest, util) {
     return {
       restrict: 'A',
       templateUrl: 'app/components/module-switcher/module-switcher.html',
       link: function(scope) {
         scope.modules = manifest.modules;
 
-        scope.$watch($stateParams, function() {
+        scope.getUrl = function(module) {
+          if (module.redirectTo) {
+            return module.redirectTo;
+          }
+
+          return $state.href('docs.service', {
+            module: module.id,
+            version: module.versions[0],
+            serviceId: module.defaultService
+          });
+        };
+
+        scope.$watch($state.params, function() {
           var module = util.findWhere(manifest.modules, {
-            id: $stateParams.module
+            id: $state.params.module
           });
 
           scope.selected = module.name;
